6f56c49952d94b6fcc8dac6f54f26e80aac9e47b
“Ewin” <ewinoaks@gmail.com>
2024-08-21 22:01:43 +0800

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Page = () => {
+  return (
+    <View>
+      <Text>PDATES</Text>
+    </View>
+  );
+};
+
+export default Page;
+//rnfe
